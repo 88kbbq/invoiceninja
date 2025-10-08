c95b0a306420c1dbe4ea325261de28b8ad4ac02d
Kevin <88kbbq@users.noreply.github.com>
2025-10-08 17:52:00 +0800

    Fix: Remove duplicate api middleware from routes

--- Modules/KitchenPrinter/routes/api.php
+++ Modules/KitchenPrinter/routes/api.php
@@ -7,19 +7,18 @@ use Modules\KitchenPrinter\Http\Controllers\KitchenPrintController;
 |--------------------------------------------------------------------------
 | API Routes for Kitchen Printer Module
 |--------------------------------------------------------------------------
+| Note: 'api' middleware and '/api' prefix are already added by RouteServiceProvider
 */
 
 // CloudPRNT endpoint (no auth - printer polls this)
-Route::middleware(['api'])
-    ->prefix('v1')
-    ->group(function () {
-        Route::get('kitchen/cloudprnt', [KitchenPrintController::class, 'cloudprntPoll'])
-            ->name('kitchen.cloudprnt.poll');
-        Route::post('kitchen/cloudprnt', [KitchenPrintController::class, 'cloudprntStatus'])
-            ->name('kitchen.cloudprnt.status');
-        Route::get('kitchen/test-job', [KitchenPrintController::class, 'queueTestJob'])
-            ->name('kitchen.test.job');
-    });
+Route::prefix('v1')->group(function () {
+    Route::get('kitchen/cloudprnt', [KitchenPrintController::class, 'cloudprntPoll'])
+        ->name('kitchen.cloudprnt.poll');
+    Route::post('kitchen/cloudprnt', [KitchenPrintController::class, 'cloudprntStatus'])
+        ->name('kitchen.cloudprnt.status');
+    Route::get('kitchen/test-job', [KitchenPrintController::class, 'queueTestJob'])
+        ->name('kitchen.test.job');
+});
 
 Route::middleware(['api_db', 'token_auth', 'locale'])
     ->prefix('v1')
