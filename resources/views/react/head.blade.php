1063c17e7548aeed589b16d9d4ab2944631240e2
Kevin <88kbbq@users.noreply.github.com>
2025-10-12 03:34:47 +0800

    Update React build file references in Blade template

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
-<link rel="stylesheet" href="{{ asset('react/index-30c22521.css') }}">
-<script type="module" crossorigin src="{{ asset('react/index-f80e709f.js') }}"></script>
+<link rel="stylesheet" href="{{ asset('react/index-BDngorsm.css') }}">
+<script type="module" crossorigin src="{{ asset('react/index-s-yLaeRw.js') }}"></script>
