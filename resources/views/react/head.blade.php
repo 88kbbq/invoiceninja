c7f12c73d4d896edfc9bcc9fd8a2fa393d88c6ea
Kevin <88kbbq@users.noreply.github.com>
2025-10-27 13:02:29 +0800

    Update React bundle references for Taiwan e-invoice auto-refresh feature

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="{{ asset('react/index-DBvx1CsZ.css') }}">
-<script type="module" crossorigin src="{{ asset('react/index-BX3XfzYV.js') }}"></script>
+<script type="module" crossorigin src="{{ asset('react/index-Bno9O8Kr.js') }}"></script>
